1446c441b331aa7ec08d07154d67f7bbb026355a
Harsh Thakur <harshthakur9030@gmail.com>
2023-12-13 07:34:58 +0530

    move to build
    Signed-off-by: Harsh Thakur <harshthakur9030@gmail.com>

---
+++ build/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+  
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=nixos-22.11";
+    };
+  };
+
+  outputs = { 
+  self, 
+  nixpkgs,
+  }: let
+  system = "aarch64-linux";
+  pkgs = import nixpkgs { inherit system; }; 
+  in {
+    packages.${system} = {
+      mypackage = pkgs.callPackage ./default.nix {
+       go_1_20 = pkgs.go_1_20;
+      };
+      default = self.packages.${system}.mypackage;
+    }; 
+  };
+}
