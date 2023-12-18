7facfd462ea8b6d65fb6daebe5f2133f89e2cad5
Harsh Thakur <harshthakur9030@gmail.com>
2023-12-18 10:19:52 +0530

    Add support for multiple systems
    Signed-off-by: Harsh Thakur <harshthakur9030@gmail.com>

--- build/flake.nix
+++ build/flake.nix
@@ -2,37 +2,36 @@
   description = "A very basic flake";
   
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { 
-  self, 
-  nixpkgs,
-  }: let
-  system = "aarch64-linux";
-  pkgs = import nixpkgs { inherit system; }; 
+  outputs = { self, nixpkgs }: let
+    supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+    forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+      pkgs = import nixpkgs { inherit system; };
+    });
   in {
-    packages.${system} = {
-      mypackage = pkgs.callPackage ./default.nix {
-       go_1_21 = pkgs.go_1_21;
+    packages = forEachSupportedSystem ({ pkgs }: {
+      default = pkgs.callPackage ./default.nix {
+        go_1_21 = pkgs.go_1_21;
       };
-      default = self.packages.${system}.mypackage;
+    });
 
-      devShells = pkgs.mkShell {
-         packages = with pkgs; [
-            go_1_19 # Go 1.19
-            gotools # Go tools like goimports, godoc, and others
-            redis
-          ];
+    devShells = forEachSupportedSystem ({ pkgs }: {
+      devShell = pkgs.mkShell {
+        # The Nix packages provided in the environment
+        packages = with pkgs; [
+          go_1_19 # Go 1.19
+          gotools # Go tools like goimports, godoc, and others
+        ];
       };
+    });
 
-      runtimeEnv = pkgs.buildEnv {
-        name = "my-env";
-        paths = with pkgs; [ bash curl  ];
+    runtimeEnvs = forEachSupportedSystem ({ pkgs }: {
+      runtime = pkgs.buildEnv {
+        name = "runtimeenv";
+        paths = with pkgs; [ bash curl ];
       };
-
-    }; 
+    });
   };
 }
